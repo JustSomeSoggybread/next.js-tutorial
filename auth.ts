de511d16e2a365eb0c27e90094f6a95d661b5504
JustSomeSoggyBread <evanhlee@outlook.com>
2025-07-16 17:46:04 -0700

    basic implemention of google oauth

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
+import Google from "next-auth/providers/google";
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  
@@ -17,8 +18,9 @@ async function getUser(email: string): Promise<User | undefined> {
     throw new Error('Failed to fetch user.');
   }
 }
- 
-export const { auth, signIn, signOut } = NextAuth({
+
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
@@ -38,5 +40,7 @@ export const { auth, signIn, signOut } = NextAuth({
         return null;
       },
     }),
+
+    Google
   ],
 });
